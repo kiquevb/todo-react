40073861ccc925cfa50465b47e700ebac58cc76c
Kiquevb <luisenrique.vergarabadillo@gmail.com>
2021-08-16 11:45:37 -0500

    Delete clock component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { default as Button } from "./Button/Button";
 export { default as Task } from "./Tasks/Task";
 export { default as Tasks } from "./Tasks/Tasks";
 export { default as Date } from "./Date/DateComponent";
-export { default as Clock } from "./Date/Clock";
 export { default as TaskForm } from "./Form/Form";
